e7ec4abcd497af6e7470d4ba97ebb41af175991e
leonardsan222 <73238421+leonardsan222@users.noreply.github.com>
2022-02-16 03:51:49 +0900

    タッチ操作とキー入力を共存させました。

--- Assets/FripperController.cs
+++ Assets/FripperController.cs
@@ -24,68 +24,71 @@ public class FripperController : MonoBehaviour
 
     void Update()
     {
-        /*
-        if ((Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S)) && tag == "LeftFripperTag")
+        if (Input.touchCount == 0)
         {
-            SetAngle(this.flickAngle);
-        }
-
-        if ((Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.S)) && tag == "RightFripperTag")
-        {
-            SetAngle(this.flickAngle);
-        }
-
-        if ((Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S)) && tag == "LeftFripperTag")
-        {
-            SetAngle(this.defaultAngle);
-        }
-
-        if ((Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.S)) && tag == "RightFripperTag")
-        {
-            SetAngle(this.defaultAngle);
-        }
-        */
-
-
-        // タッチしている指の数だけ繰り返し
-        foreach (Touch touch in Input.touches)
-        {
-            // タッチの開始を検知
-            if (touch.phase == TouchPhase.Began)
+            if ((Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S)) && tag == "LeftFripperTag")
             {
-                // タッチの開始が画面の左側のとき左側指リストに指のIDを追加
-                if (touch.position.x < 540.0f && tag == "LeftFripperTag")
+                SetAngle(this.flickAngle);
+            }
+
+            if ((Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.S)) && tag == "RightFripperTag")
+            {
+                SetAngle(this.flickAngle);
+            }
+
+            if ((Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S)) && tag == "LeftFripperTag")
+            {
+                SetAngle(this.defaultAngle);
+            }
+
+            if ((Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.S)) && tag == "RightFripperTag")
+            {
+                SetAngle(this.defaultAngle);
+            }
+        }
+        
+
+        else
+        {
+            // タッチしている指の数だけ繰り返し
+            foreach (Touch touch in Input.touches)
+            {
+                // タッチの開始を検知
+                if (touch.phase == TouchPhase.Began)
                 {
-                    SetAngle(this.flickAngle);
-                    leftFingerList.Add(touch.fingerId);
+                    // タッチの開始が画面の左側のとき左側指リストに指のIDを追加
+                    if (touch.position.x < 540.0f && tag == "LeftFripperTag")
+                    {
+                        SetAngle(this.flickAngle);
+                        leftFingerList.Add(touch.fingerId);
+                    }
+                    // タッチの開始が画面の右側のとき右側指リストに指のIDを追加
+                    if (touch.position.x >= 540.0f && tag == "RightFripperTag")
+                    {
+                        SetAngle(this.flickAngle);
+                        rightFingerList.Add(touch.fingerId);
+                    }
                 }
-                // タッチの開始が画面の右側のとき右側指リストに指のIDを追加
-                if (touch.position.x >= 540.0f && tag == "RightFripperTag")
+
+                // 指が離れたときリストからその指のIDを削除
+                if (touch.phase == TouchPhase.Ended)
                 {
-                    SetAngle(this.flickAngle);
-                    rightFingerList.Add(touch.fingerId);
+                    leftFingerList.Remove(touch.fingerId);
+                    rightFingerList.Remove(touch.fingerId);
                 }
             }
 
-            // 指が離れたときリストからその指のIDを削除
-            if (touch.phase == TouchPhase.Ended)
+            // 左でタッチし始めた指が画面からすべて離れたとき
+            if (leftFingerList.Count == 0 && tag == "LeftFripperTag")
             {
-                leftFingerList.Remove(touch.fingerId);
-                rightFingerList.Remove(touch.fingerId);
+                SetAngle(this.defaultAngle);
+            }
+            // 右でタッチし始めた指が画面からすべて離れたとき
+            if (rightFingerList.Count == 0 && tag == "RightFripperTag")
+            {
+                SetAngle(this.defaultAngle);
             }
         }
-
-        // 左でタッチし始めた指が画面からすべて離れたとき
-        if (leftFingerList.Count == 0 && tag == "LeftFripperTag")
-        {
-            SetAngle(this.defaultAngle);
-        }
-        // 右でタッチし始めた指が画面からすべて離れたとき
-        if (rightFingerList.Count == 0 && tag == "RightFripperTag")
-        {
-            SetAngle(this.defaultAngle);
-        }
-
     }
 
     public void SetAngle(float angle)
